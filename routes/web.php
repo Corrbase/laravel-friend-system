1a030217369fb6dda60398213e7af5dff95fec4d
corrbase <corrbase@gmail.com>
2022-09-26 19:09:16 +0400

    friends page view

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('friends', \App\Http\Controllers\FriendIndexController::class)->name('friends');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
