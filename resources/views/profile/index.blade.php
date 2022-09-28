50f1ace03f18da07a2e5cfe538d39387b46de5c9
corrbase <corrbase@gmail.com>
2022-09-28 19:01:29 +0400

    Preventing adding self as friend

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -11,58 +11,63 @@
                 <div class="p-6 bg-white border-b border-gray-200">
 
                     @auth()
-                        @if(auth()->user()->isFriendsWith($user))
-                            <div class="space-x-2">
-                                <span>
-                                    You and {{ $user->name }} are friends
-                                </span>
+                        @if(auth()->user()->id == $user->id))
 
-                                <form action="{{ route('friends.destroy', ['user_id' => $user]) }}" method="post" class="inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="text-indigo-600">Unfriend</button>
-                                </form>
-                            </div>
-                        @else
-
-                            @if(auth()->user()->hasPendingFriendRequestFor($user))
+                            @if(auth()->user()->isFriendsWith($user))
                                 <div class="space-x-2">
                                     <span>
-                                        Waiting for <span class="text-yellow-300">{{ $user->name }}</span> to accept your friend request.
+                                        You and {{ $user->name }} are friends
                                     </span>
 
                                     <form action="{{ route('friends.destroy', ['user_id' => $user]) }}" method="post" class="inline">
                                         @csrf
                                         @method('DELETE')
-                                        <button class="text-indigo-600">Cancel</button>
+                                        <button class="text-indigo-600">Unfriend</button>
                                     </form>
-                                </div>
-                            @elseif($user->hasPendingFriendRequestFor(auth()->user()))
-                                <div class="space-x-2">
-                                    <span>
-                                        {{ $user->name }} has sent you a friend request
-                                    </span>
-
-                                    <form action="{{ route('friends.patch', ['user_id' => $user]) }} " method="post" class="inline">
-                                        @csrf
-                                        @method('PATCH')
-                                        <button class="text-indigo-600 inline">Accept</button>
-                                    </form>
-
-                                    <form action="{{ route('friends.destroy', ['user_id' => $user]) }} " method="post" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="text-indigo-600 inline">Reject</button>
-                                    </form>
-
                                 </div>
                             @else
 
-                                <form action="{{ route('friends.store', ['user_id' => $user]) }}" method="post">
-                                    @csrf
-                                    <button class="text-indigo-600">Add as friend</button>
-                                </form>
+                                @if(auth()->user()->hasPendingFriendRequestFor($user))
+                                    <div class="space-x-2">
+                                        <span>
+                                            Waiting for <span class="text-yellow-300">{{ $user->name }}</span> to accept your friend request.
+                                        </span>
+
+                                        <form action="{{ route('friends.destroy', ['user_id' => $user]) }}" method="post" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="text-indigo-600">Cancel</button>
+                                        </form>
+                                    </div>
+                                @elseif($user->hasPendingFriendRequestFor(auth()->user()))
+                                    <div class="space-x-2">
+                                        <span>
+                                            {{ $user->name }} has sent you a friend request
+                                        </span>
+
+                                        <form action="{{ route('friends.patch', ['user_id' => $user]) }} " method="post" class="inline">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button class="text-indigo-600 inline">Accept</button>
+                                        </form>
+
+                                        <form action="{{ route('friends.destroy', ['user_id' => $user]) }} " method="post" class="inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="text-indigo-600 inline">Reject</button>
+                                        </form>
+
+                                    </div>
+                                @else
+
+                                    <form action="{{ route('friends.store', ['user_id' => $user]) }}" method="post">
+                                        @csrf
+                                        <button class="text-indigo-600">Add as friend</button>
+                                    </form>
+                                @endif
                             @endif
+                        @else
+                            It is your account
                         @endif
                     @endauth
 
